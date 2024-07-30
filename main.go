506b708dc44059c536ebf3361d3b9f3bac0b082c
rickyyyy <rickywong0625@gmail.com>
2024-07-30 21:20:07 +0800

    feat: read content from csv file

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 const default_file = "problems.csv"
@@ -14,13 +17,35 @@ type QuizConfig struct {
 // initialize quiz config
 func (qc *QuizConfig) init() {
 	flag.StringVar(&qc.file, "file", default_file, "path to file")
-
 	flag.Parse()
+}
+
+func (qc *QuizConfig) readFile() ([][]string, error) {
+	// open file
+	f, err := os.Open(qc.file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	defer f.Close()
+
+	// read content from csv file
+	csvReader := csv.NewReader(f)
+	lines, err := csvReader.ReadAll()
+	if err != nil {
+		return nil, fmt.Errorf("failed to read CSV data: %w", err)
+	}
+
+	return lines, nil
 }
 
 func main() {
 	var config QuizConfig
 
 	config.init()
-	fmt.Println(config.file)
+	lines, err := config.readFile()
+	if err != nil {
+		log.Fatalf("error reading file: %v", err)
+	}
+
+	fmt.Printf("Data: %+v\n", lines)
 }
