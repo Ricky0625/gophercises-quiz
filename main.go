3280bb8be231598e5b9c27ff28529ab80ce9b415
rickyyyy <rickywong0625@gmail.com>
2024-07-30 20:33:58 +0800

    feat: flags parsing, -file flag

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+const default_file = "problems.csv"
+
+type QuizConfig struct {
+	file string
+}
+
+// initialize quiz config
+func (qc *QuizConfig) init() {
+	flag.StringVar(
+		&qc.file,
+		"file",
+		default_file,
+		"path to file",
+	)
+
+	flag.Parse()
+}
+
+func main() {
+	var config QuizConfig
+
+	config.init()
+	fmt.Println(config.file)
+}
