aeaecbf5bb5ce2731bb28686da378fbe9a353c85
rickyyyy <rickywong0625@gmail.com>
2024-07-30 21:40:18 +0800

    feat: parse lines into problems

--- main.go
+++ main.go
@@ -10,20 +10,22 @@ import (
 
 const defaultFile = "problems.csv"
 
-type quizConfig struct {
+type QuizConfig struct {
 	file string
 }
 
-type problem struct {
+type Problem struct {
+	question string
+	answer   string
 }
 
 // initialize quiz config
-func (qc *quizConfig) init() {
+func (qc *QuizConfig) init() {
 	flag.StringVar(&qc.file, "file", defaultFile, "path to file")
 	flag.Parse()
 }
 
-func (qc *quizConfig) readFile() ([][]string, error) {
+func (qc *QuizConfig) readFile() ([][]string, error) {
 	// open file
 	f, err := os.Open(qc.file)
 	if err != nil {
@@ -41,8 +43,27 @@ func (qc *quizConfig) readFile() ([][]string, error) {
 	return lines, nil
 }
 
+/*
+Do not use make([]problem, 0, len(lines))
+Issue: The original code used append to add elements to the problems slice, which caused a slice with twice the length of lines, leading to inefficient memory usage.
+Solution: Use make with a length of 0 and a capacity of len(lines), or directly initialize the slice with the appropriate length to avoid unnecessary allocation.
+*/
+
+func parseLines(lines [][]string) []Problem {
+	problems := make([]Problem, 0, len(lines))
+
+	for _, line := range lines {
+		problem := Problem{
+			question: line[0],
+			answer:   line[1],
+		}
+		problems = append(problems, problem)
+	}
+	return problems
+}
+
 func main() {
-	var config quizConfig
+	var config QuizConfig
 
 	config.init()
 	lines, err := config.readFile()
@@ -50,5 +71,8 @@ func main() {
 		log.Fatalf("error reading file: %v", err)
 	}
 
-	fmt.Printf("Data: %+v\n", lines)
+	problems := parseLines(lines)
+	for _, problem := range problems {
+		fmt.Printf("q: %s, a: %s\n", problem.question, problem.answer)
+	}
 }
